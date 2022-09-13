6673623562a29a495d564184d73f668d9b88cfb9
Pavel Tropinov <pavel.tropinov@gmail.com>
2022-09-13 16:51:53 +0300

    Little improvements

--- src/main/java/ru/aneux/library/SpringLibraryApplication.java
+++ src/main/java/ru/aneux/library/SpringLibraryApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringMvcLibraryAppApplication {
+public class SpringLibraryApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(SpringMvcLibraryAppApplication.class, args);
+		SpringApplication.run(SpringLibraryApplication.class, args);
 	}
 }
